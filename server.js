fe1e6b277e922670b16292a1e4ec7e8dc695af25
Kyle <kylemphillips65@yahoo.com>
2023-02-03 18:56:50 -0500

    finished the new employee function and add the new role function

--- server.js
+++ server.js
@@ -7,23 +7,23 @@ const db = mysql.createConnection({
     user: "root",
     database: "employee_db",
 },
-console.log("You are now coonected to employee_db")
+    console.log("You are now coonected to employee_db")
 );
 
 
 const chooseAll = async (table) => {
     return await db.promise().query("SLECT * FROM " + table);
-}; 
+};
 
 const insert = (table, info) => {
-    db.query("INSERT INTO ?? SET ?", [table,info], (err) => {
+    db.query("INSERT INTO ?? SET ?", [table, info], (err) => {
         if (err) return console.error(err);
         console.log("\nSuccessfully created!\n");
         init();
     });
 };
 
-
+// ------ NEW EMPLOYEE --------
 const newEmployee = async () => {
     const [roleInfo] = await chooseAll("role");
     const [employeeInfo] = await chooseAll("employee");
@@ -33,74 +33,90 @@ const newEmployee = async () => {
             value: role.id
         }
     });
-}
+    const mangers = employeeInfo.map(employee => {
+        return {
+            name: employee.first_name + " " + employee.last_name,
+            value: employee.id
+        }
+    });
+    const answers = await prompt([
+        {
+            name: "first_name",
+            message: "New hire first name?"
+        },
+        {
+            name: "last_name",
+            message: "New hire last name?"
+        },
+        {
+            type: "rawlist",
+            name: "role_id",
+            message: "New hire role?",
+            choices: roles,
+        },
+        {
+            type: "rawlist",
+            name: "manager_id",
+            message: "Who is your manager?",
+            choices: mangers,
+        }
+    ])
+    insert("employee", answers);
+};
 
-const answers = await prompt ([
-    {
-        name: "first_name",
-        message: "New hire first name?"
-    },
-    {
-        name: "last_name",
-        message: "New hire last name?"
-    },
-    {
-        type: "rawlist",
-        name: "role_id",
-        message: "New hire role?",
-        choices: roles,
-    },
-    {
-        type: "rawlist",
-        name: "manager_id",
-        message: "Who is your manager?",
-        choices: mangers,
+
+// -------- NEW ROLE ---------
+
+const newRole = async () => {
+    const [departmentInfo] = await chooseAll("department");
+    const [roleInfo] = await chooseAll("role");
+    const departments = departmentInfo.map(department => {
+        return {
+            name: department.name,
+            value: department.id
+        }
+    });
+    const anwsers = await prompt([
+        {
+            name: "title",
+            message: "What is the position title?"
+        },
+        {
+            name: "salary",
+            message: "What is the salary for the new position?"
+        },
+        {
+            type: "rawlist",
+            name: "department_id",
+            message: "What department does this poistion belong in?",
+            choices: departments,
+        }
+    ])
+    insert("role",answers);
+};
+
+
+
+
+    const anwsers = await prompt([{
+        name: "name",
+        message: "New department name?"
     }
-])
+    ])
 
 
-
-
-
-const anwsers = await prompt([
-    {
-        name: "title",
-        message: "What is the position title?"
-    },
-    {
-        name: "salary",
-        message: "What is the salary for the new position?"
-    },
-    {
+    const answers = await prompt({
         type: "rawlist",
-        name: "department_id",
-        message: "What department does this poistion belong in?",
-        choices: departments,
-    }
-])
-
-
-
-
-const anwsers = await prompt ([{
-    name: "name",
-    message: "New department name?"
-}
-])
-
-
-const answers = await prompt ({
-    type: "rawlist",
-    message: "Please pick one to add to.",
-    chocies: [
-        "View all Departments",
-        "View all Employess",
-        "View all Roles",
-        "Add a Department",
-        "Add an Employee",
-        "Add a Role",
-    ]
-})
+        message: "Please pick one to add to.",
+        chocies: [
+            "View all Departments",
+            "View all Employess",
+            "View all Roles",
+            "Add a Department",
+            "Add an Employee",
+            "Add a Role",
+        ]
+    })
 
 
 
